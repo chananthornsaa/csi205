1f6cff9f585ea19ee4e128c819438c88975f3596
chananthornsaa <chananthorn.saa@spumail.net>
2025-11-11 18:30:34 +0700

    delete source code

--- src/components/Header.jsx
+++
@@ -1,9 +0,0 @@
-function Header() {
-  return (
-    <header className="bg-primary text-white py-3 text-center fs-4 fw-bold">
-      Welcome to My React App
-    </header>
-  );
-}
-
-export default Header;
