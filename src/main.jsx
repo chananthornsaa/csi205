14c045a15a3c18aa436a49d7d4912bf0b577c8cd
chananthornsaa <chananthorn.saa@spumail.net>
2025-11-11 18:13:04 +0700

    fix3

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,12 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
 
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
+document.title = 'CSI205 FrontEnd'
 
-
-document.title = 'CSI205 FrontEnd';
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
     <App />
-  </React.StrictMode>
-);
+  </StrictMode>,
+)
